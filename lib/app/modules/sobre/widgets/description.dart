f2fd74e6eada3304b1bda61881ad4cd5ce371a60
Eric Pereira <esilva@gyramais.com>
2024-07-17 18:23:17 -0300

    correção do sobre

--- lib/app/modules/sobre/widgets/description.dart
+++ lib/app/modules/sobre/widgets/description.dart
@@ -16,9 +16,10 @@ class Description extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             TextCustom(
-              '''Sou Técnico em Informática e graduado em Análise e Desenvolvimento de Sistemas pela Estácio, atuando como desenvolvedor há alguns anos. Minha principal ferramenta de trabalho é o Node.js. Embora tenha conhecimento em desenvolvimento front-end, optei por me especializar no back-end, especialmente no ecossistema do Node.js.\n
+              '''Sou Técnico em Informática e graduado em Análise e Desenvolvimento de Sistemas pela Estácio. Trabalho com desenvolvimento há alguns anos, comecei minha carreira com 18 anos como desenvolvedor desktop utilizando Delphi, para contrução de aplicações. Logo depois tive meu primeiro contato com php e comecei a desenvolver minhas primeiras aplicações web.\n
+Ao longo dos anos fui conhecendo novas tecnologias como Python, Java, Kotlin, React, Flutter, entre outras. Porém optei por me especializar no back-end, especialmente no ecossistema do Node.js.\n
 Tenho experiência em desenvolvimento de aplicações robustas e escaláveis, além de boas práticas de programação, como testes automatizados e integração contínua. Minha jornada profissional inclui colaboração em diversos projetos, desde startups até grandes empresas, sempre buscando entregar soluções eficientes e de alta qualidade.\n
-Atualmente, estou consolidando meu conhecimento em Node.js, Next.js e Flutter, com o objetivo de ampliar minha expertise e contribuir ainda mais para projetos inovadores e desafiadores. 🚀''',
+Atualmente, estou consolidando meu conhecimento em Node e Flutter, com o objetivo de ampliar minha expertise e contribuir ainda mais para projetos inovadores e desafiadores. 🚀''',
               fontSize: 20,
               fontWeight: FontWeight.w400,
               textHeight: 1.2,
