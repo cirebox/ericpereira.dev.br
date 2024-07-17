f4e0a844540152aff6f8973f52098d4e6b29595b
Eric Pereira <esilva@gyramais.com>
2024-07-17 18:31:15 -0300

    correção component do portfolio

--- lib/app/modules/portfolio/presenter/widgets/project_card.dart
+++ lib/app/modules/portfolio/presenter/widgets/project_card.dart
@@ -33,53 +33,61 @@ class ProjectCard extends StatelessWidget {
         }
       },
       child: Card(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: SizedBox(
-            height: 170,
-            width: 280,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                SizedBox(
-                  height: 100,
-                  child: Image.asset(
-                    imageUrl,
-                    fit: BoxFit.fill,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 20,
-                    vertical: 10,
-                  ),
-                  child: SizedBox(
-                    height: 50,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        TextCustom(
-                          name,
-                          fontSize: 24,
-                          color: Colors.black,
-                          fontWeight: FontWeight.w600,
-                        ),
-                        const SizedBox(
-                          height: 5,
-                        ),
-                        TextCustom(
-                          'Lançado em $launch',
-                          color: Colors.black,
-                          fontSize: 18,
-                          fontWeight: FontWeight.w300,
-                        ),
-                      ],
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white.withOpacity(0.8),
+            // border: Border.all(
+            //   color: Colors.black.withOpacity(0.1),
+            // ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: SizedBox(
+              height: 170,
+              width: 280,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  SizedBox(
+                    height: 100,
+                    child: Image.asset(
+                      imageUrl,
+                      fit: BoxFit.fill,
                     ),
                   ),
-                )
-              ],
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 20,
+                      vertical: 10,
+                    ),
+                    child: SizedBox(
+                      height: 50,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          TextCustom(
+                            name,
+                            fontSize: 24,
+                            color: Colors.black,
+                            fontWeight: FontWeight.w600,
+                          ),
+                          const SizedBox(
+                            height: 5,
+                          ),
+                          TextCustom(
+                            'Lançado em $launch',
+                            color: Colors.black,
+                            fontSize: 18,
+                            fontWeight: FontWeight.w300,
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         ),
